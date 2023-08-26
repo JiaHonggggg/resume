3948331adaf5f4504985b70ff57961ff28c02470
Json <jiahong@webglsoft.com>
2023-08-26 18:36:51 +0800

    update skill component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,75 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume';
+
+  skills = [
+    {
+      title: 'Front-end',
+      content: [
+        {
+          name: 'Angular / AngularJS',
+          icon: ''
+        },
+        {
+          name: 'Vue',
+          icon: ''
+        },
+        {
+          name: 'Bootstrap',
+          icon: ''
+        },
+        {
+          name: 'Html',
+          icon: ''
+        },
+        {
+          name: 'CSS',
+          icon: ''
+        },
+        {
+          name: 'Javascript / jQuery',
+          icon: ''
+        }
+      ]
+      
+    },
+    {
+      title: 'Back-end',
+      content: [
+        {
+          name: 'Laravel',
+          icon: ''
+        },
+        {
+          name: 'PHP',
+          icon: ''
+        },
+        {
+          name: 'MS-SQL',
+          icon: ''
+        },
+        {
+          name: 'MySQL / MariaDB',
+          icon: ''
+        }
+      ]
+    },
+    {
+      title: 'Others',
+      content: [
+        {
+          name: 'GitLab (CI/CD)',
+          icon: ''
+        },
+        {
+          name: 'Nginx',
+          icon: ''
+        },
+        {
+          name: 'Docker',
+          icon: ''
+        }
+      ]
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SkillComponent } from './skill/skill.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SkillComponent
+    SkillComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
