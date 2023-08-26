b9779554d9a211211182152d602eedab1d093dc2
Json <jiahong@webglsoft.com>
2023-08-26 18:11:47 +0800

    create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SkillComponent } from './skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
